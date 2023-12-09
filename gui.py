8300ee3f31269835a972fe3ef258637d3edbb605
YeonJun Kim <tim99416@gmail.com>
2023-12-09 17:52:55 +0900

    mp3와 mp4 형식을 바꿔서 다운로드 받을 수 있게 만듦

--- gui.py
+++ gui.py
@@ -23,8 +23,11 @@ def download_youtube_video():
         # 전역 변수에 이전 다운로드 경로 저장
         previous_download_path = download_path
 
-        # 다운로드 함수 호출
-        download_video(youtube_url, download_path, update_status_label)
+        # 선택된 다운로드 형식 가져오기 (mp3 또는 mp4)
+        chosen_format = download_format.get()
+
+        # 다운로드 함수 호출 시 선택된 형식 전달
+        download_video(youtube_url, download_path, update_status_label, chosen_format)
 
         # 다운로드 완료 후 폴더 열기 버튼 활성화
         open_folder_button.config(state=tk.NORMAL)
@@ -32,6 +35,13 @@ def download_youtube_video():
     # 쓰레드 생성 및 시작
     download_thread = threading.Thread(target=download)
     download_thread.start()
+
+# 사용자의 선택에 따라 다운로드 형식을 업데이트하는 함수
+def update_download_format():
+    global download_format
+
+    # 사용자가 선택한 값이 어떤 형식인지 이미 download_format에 저장되어 있음
+    chosen_format = download_format
 
 def update_status_label(status):
     status_label.config(text=status)
@@ -49,6 +59,17 @@ def open_download_folder():
 # GUI 생성
 app = tk.Tk()
 app.title('YouTube Downloader')
+
+# 사용자의 다운로드 형식 선택을 저장하는 변수
+download_format = tk.StringVar()
+download_format.set("mp4")  # 디폴트 형식을 mp4로 설정
+
+# 다운로드 형식을 선택하기 위한 라디오 버튼
+mp4_radio = tk.Radiobutton(app, text="MP4", variable=download_format, value="mp4", command=update_download_format)
+mp4_radio.grid(row=1, column=0, padx=10)
+
+mp3_radio = tk.Radiobutton(app, text="MP3", variable=download_format, value="mp3", command=update_download_format)
+mp3_radio.grid(row=1, column=1, padx=10)
 
 # 레이블과 엔트리 위젯 추가
 url_label = tk.Label(app, text='유튜브 동영상 URL 입력:')
