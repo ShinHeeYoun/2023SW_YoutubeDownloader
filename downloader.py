3e074e687505b7767a852b0226c2806f5e5ddef1
seyoung <bsy6940@naver.com>
2023-12-10 13:24:08 +0900

    main에서 병합하여 진행도추가

--- downloader.py
+++ downloader.py
@@ -8,7 +8,7 @@ def on_progress(stream, chunk, bytes_remaining):
     print(f"다운로드 중... {percentage:.2f}% 완료", end='\r', flush=True)
 
 
-def download_video(youtube_url, download_path, status_callback, download_format):
+def download_video(youtube_url, download_path, status_callback, update_progress_label, download_format):
     try:
          # YouTube 비디오 객체 생성
         yt = YouTube(youtube_url, on_progress_callback=on_progress)
--- gui.py
+++ gui.py
@@ -4,70 +4,119 @@ import tkinter as tk
 from tkinter import filedialog
 import threading
 import os
-from downloader import download_video
+from pytube import YouTube
+from pytube.exceptions import PytubeError
 
 # 전역 변수로 이전 다운로드 경로를 저장할 변수 추가, 디폴트는 실행기 위치치
 previous_download_path = None
 
+def update_progress_label(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage = (bytes_downloaded / total_size) * 100
+    progress_label.config(text=f'진행도: {percentage:.2f}%')
+    app.event_generate("<<UpdateStatus>>", when="tail", data=f'다운로드 중... {percentage:.2f}% 완료')
+    app.update()  # Tkinter 업데이트 처리
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage = (bytes_downloaded / total_size) * 100
+    print(f"다운로드 중... {percentage:.2f}% 완료", end='\r', flush=True)
+
+def download_video(youtube_url, download_path, status_callback, progress_callback, download_format):
+    try:
+        yt = YouTube(youtube_url, on_progress_callback=progress_callback)
+        status_callback(f'찾는 중: {yt.title}')
+
+        filename = None
+
+        if download_format == "mp4":
+            selected_stream = yt.streams.get_highest_resolution()
+            filename = f"{yt.title}.mp4"
+        elif download_format == "mp3":
+            selected_stream = yt.streams.filter(only_audio=True).first()
+            filename = f"{yt.title}.mp3"
+        else:
+            status_callback("에러: 지원하지 않는 다운로드 형식입니다.")
+            return
+
+        status_callback(f'다운로드 중: {yt.title}')
+        selected_stream.download(download_path, filename=filename)
+        status_callback('다운로드 완료!')
+
+    except PytubeError as e:
+        if 'invalid' in str(e).lower():
+            status_callback('에러: 유효한 YouTube 동영상 URL이 아닙니다.')
+        elif 'unavailable' in str(e).lower():
+            status_callback('에러: 선택한 동영상은 유효하지 않거나 삭제되었습니다.')
+        elif 'private' in str(e).lower():
+            status_callback('에러: 선택한 동영상은 비공개 상태입니다.')
+        elif 'livestream' in str(e).lower():
+            status_callback('에러: 선택한 동영상은 라이브 스트리밍 중이어서 다운로드할 수 없습니다.')
+        else:
+            status_callback(f'에러: {str(e)}')
+
 def download_youtube_video():
     def download():
-        # 전역 변수를 사용하여 이전 다운로드 경로 저장
         global previous_download_path
 
-        # 유튜브 영상 URL을 입력 받음
         youtube_url = url_entry.get()
-
-        # 다운로드 위치 선택
         download_path = filedialog.askdirectory()
-
-        # 전역 변수에 이전 다운로드 경로 저장
         previous_download_path = download_path
+        chosen_format = download_format.get()
 
-        # 다운로드 함수 호출
-        download_video(youtube_url, download_path, update_status_label, 'mp4')
+        download_video(youtube_url, download_path, update_status_label, update_progress_label, chosen_format)
 
-        # 다운로드 완료 후 폴더 열기 버튼 활성화
         open_folder_button.config(state=tk.NORMAL)
 
-    # 쓰레드 생성 및 시작
     download_thread = threading.Thread(target=download)
     download_thread.start()
+
+def update_download_format():
+    global download_format
+
+    chosen_format = download_format
 
 def update_status_label(status):
     status_label.config(text=status)
 
 def open_download_folder():
-    # 전역 변수로부터 이전 다운로드 경로 가져오기
     global previous_download_path
 
-    # 이전 다운로드 경로가 있을 경우 폴더 열기
     if previous_download_path:
         os.system(f'explorer {os.path.realpath(previous_download_path)}')
     else:
         update_status_label("이전 다운로드 폴더가 없습니다.")
 
-# GUI 생성
 app = tk.Tk()
 app.title('YouTube Downloader')
 
-# 레이블과 엔트리 위젯 추가
+download_format = tk.StringVar()
+download_format.set("mp4")
+
+mp4_radio = tk.Radiobutton(app, text="MP4", variable=download_format, value="mp4", command=update_download_format)
+mp4_radio.grid(row=1, column=0, padx=10)
+
+mp3_radio = tk.Radiobutton(app, text="MP3", variable=download_format, value="mp3", command=update_download_format)
+mp3_radio.grid(row=1, column=1, padx=10)
+
 url_label = tk.Label(app, text='유튜브 동영상 URL 입력:')
-url_label.grid(row=0, column=0, pady=10, padx=10)  # padx 옵션 추가
+url_label.grid(row=0, column=0, pady=10, padx=10)
 
 url_entry = tk.Entry(app, width=40)
 url_entry.grid(row=0, column=1, pady=10)
 
-# Download 버튼을 URL 입력 창 바로 오른쪽에 위치
 download_button = tk.Button(app, text='다운로드', command=download_youtube_video)
-download_button.grid(row=0, column=2, padx=20, pady=20)  # padx 옵션 추가
+download_button.grid(row=0, column=2, padx=20, pady=20)
 
-# 다운로드 완료 후 폴더 열기 버튼
 open_folder_button = tk.Button(app, text='폴더 열기', command=open_download_folder, state=tk.DISABLED)
-open_folder_button.grid(row=1, column=2, padx=20, pady=20)  # padx 옵션 추가
+open_folder_button.grid(row=1, column=2, padx=20, pady=20)
 
-# 상태 표시 레이블 추가
 status_label = tk.Label(app, text='')
 status_label.grid(row=2, column=0, columnspan=3)
 
-# GUI 실행
+progress_label = tk.Label(app, text='진행도: 0%', pady=10)
+progress_label.grid(row=3, column=0, columnspan=3)
+
 app.mainloop()
